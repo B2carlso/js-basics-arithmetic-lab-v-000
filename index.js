3a44ad52fca2a67c5f87aa5d59e7609a80715555
Brad Carlson <b2carlso@yahoo.com>
2018-04-07 01:47:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,5 @@ const ageIsValid = Number.isInteger(currentAge);
 const randomNumber = Math.random(0, 20);
 
 const randomInteger = Math.floor(randomNumber);
+
+const randomUserID = Math.random(1000000001, 1000000020);
