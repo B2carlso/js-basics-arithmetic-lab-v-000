2c52e2bd89e806f427144d878f3346ad14392898
Brad Carlson <b2carlso@yahoo.com>
2018-04-07 01:48:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ const randomNumber = Math.random(0, 20);
 
 const randomInteger = Math.floor(randomNumber);
 
-let randomUserID = Math.random(1000000001, 1000000020);
+const randomUserID = Math.random(1000000001, 1000000020);
